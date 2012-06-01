64ce547a11563a590ba75c48b5cf21f91cce5c2c
ubuntu64s <ubuntu64s@ubuntu64s-desktop.(none)>
2012-06-01 09:54:42 +0200

    ..

---
+++ src/g1.go
@@ -0,0 +1,6 @@
+package main
+//import mmm "git@github.com/git-hanjo/p001/src/myLib/f1.go"
+import mmm "git@github.com/git-hanjo/p001/tree/master/src/myLib/f1.go"
+func main(){
+  mmm.MM("der Text")
+}
