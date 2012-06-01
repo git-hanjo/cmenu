981b3057babea8cd22946c819201bcde904f1b10
Ubuntu64 <ubuntu64@ubuntu64-desktop.(none)>
2012-06-01 08:27:20 +0200

    1.Coomit für go

---
+++ src/go.go
@@ -0,0 +1,6 @@
+package main
+import "myLib"
+
+func main(){
+  myLib.MM("Das ist die Message")
+}
---
+++ src/myLib/f1.go
@@ -0,0 +1,8 @@
+package myLib
+import "fmt"
+
+func MM(asMsg string){
+  fmt.Println("-------------------")
+  fmt.Printf("Msg: %s\n",asMsg)
+  fmt.Println("-------------------")
+}
