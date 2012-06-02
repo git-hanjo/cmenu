f3c7645ed3efb07608199e18b04ed9aa3c80fbe9
Ubuntu64 <ubuntu64@ubuntu64-desktop.(none)>
2012-06-02 11:57:31 +0200

    modify

--- src/main.go
+++ src/main.go
@@ -1,38 +1,58 @@
 package main
 import ("fmt";"strconv")
 
-type tMnuStruct struct{
+const ver="0.0.0"
+type tMenuItem struct{
 	cmd    string
 	note   string
 	group  int
-     }
-type  tslCmd []tMnuStruct
-var   slCmd  tslCmd
+      }
+type  tMenu struct{
+        item []tMenuItem
+	len  int
+      }
+var   vMenu  tMenu
 
-func (sl tslCmd) listMenu(){
+func (v *tMenu) listItems(){
   len1 := 0
   len2 := 0
-  for i := 0; i<len(sl);i++{
-    if len1 < len(sl[i].cmd)  { len1 = len(sl[i].cmd) }
-    if len2 < len(sl[i].note) { len2 = len(sl[i].note) }
+  for i := 0; i<=v.len;i++ {
+    if len1 < len(v.item[i].cmd)  { len1 = len(v.item[i].cmd) }
+    if len2 < len(v.item[i].note) { len2 = len(v.item[i].note) }
   }
-  for i := 0; i<10; i++ {
-//    if len(slCmd[i]) > nil {
-      fmt.Printf("%3d %-"+strconv.FormatInt(int64(len1),10)+"s %-"+strconv.FormatInt(int64(len2),10)+"s %2d\n", i, sl[i].cmd, sl[i].note, sl[i].group)
-//      fmt.Printf("%v\n", sl[i])
-//    }
+  for i := 0; i<=v.len; i++ {
+    fmt.Printf("│%3d %-"+strconv.FormatInt(int64(len1),10)+"s %-"+strconv.FormatInt(int64(len2),10)+"s %2d\n", i, v.item[i].cmd, v.item[i].note, v.item[i].group)
   }
 }
 
-func init(){
-  slCmd = make(tslCmd, 10)
-  i:=0; slCmd[i]=tMnuStruct{"git push -u origin master"                   ,"Repo pushen"                ,0}
-  i++;  slCmd[i]=tMnuStruct{"git clone git@github.com:git-hanjo/cmenu.git","Repo vom Server holen"      ,0}
-  i++;  slCmd[i]=tMnuStruct{"$GOPATH=pwd"                                 ,"GOPATH für currentProj"     ,0}
-  i++;  slCmd[i]=tMnuStruct{"alias m=`. ./m`"                             ,"Alias für calAlias für call",0}
+func (v *tMenu) init(){
+  v.item = make([]tMenuItem, 10)
+  i:=0; v.item[i]=tMenuItem{"git push -u origin master"                   ,"Repo auf Server pushen"     ,0}
+  i++;  v.item[i]=tMenuItem{"git clone git@github.com:git-hanjo/cmenu.git","Repo vom Server clonen"     ,0}
+  i++;  v.item[i]=tMenuItem{"$GOPATH=pwd"                                 ,"GOPATH für currentProj"     ,0}
+  i++;  v.item[i]=tMenuItem{"alias m=`. ./m`"                             ,"Alias für calAlias für call",0}
+  v.len = i
+}
+func screenLine(){
+  println("         1         2         3         4         5         6         7         8")
+  println("12345678901234567890123456789012345678901234567890123456789012345678901234567890")
 }
 
 func main() {
-  fmt.Printf("%s\n","----------------- cMenu")
-  slCmd.listMenu()
+  screenLine()
+  fmt.Printf("┌───────────────────────────────────────────────────── cMenu (v)%s ───\n",ver)
+  vMenu.init()
+  vMenu.listItems()
+  var i int
+  fmt.Scan(&i)
+  fmt.Println("read number", i, "from stdin")
 }
+/*
+//─│┌┬┐├┼┤└┴┘
+ tMenu
+ ├──[]tMenuItem
+ │  ├── cmdi
+ │  ├── note
+ │  └── group
+ └── len
+ */
